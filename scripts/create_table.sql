17d0e7242045f85966439768b5946e1646731818
sumana2001 <sumana.basu2001@gmail.com>
2022-03-07 13:31:04 +0530

    Create Table Script

---
+++ scripts/create_table.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE IF NOT EXISTS `marvel` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `marvel`;
+CREATE TABLE `superheroes` (
+  `superhero_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL DEFAULT ''
+  `color` varchar(30) NOT NULL,
+  `noOfMovies` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
