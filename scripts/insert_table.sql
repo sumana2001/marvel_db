16f966c8b46783969d9c52583f61332fec24f68c
sumana2001 <sumana.basu2001@gmail.com>
2022-03-08 11:32:49 +0530

    Inserted Values

---
+++ scripts/insert_table.sql
@@ -0,0 +1,12 @@
+INSERT INTO `superheroes` (`name`,`color`,`noOfMovies`)
+VALUES
+  ("Iron Man", "Gold", 11),
+  ("Captain America","Blue", 11),
+  ("Thor","Dark Grey", 7),
+  ("Hulk","Green", 9),
+  ("Black Widow","Red", 8),
+  ("Hawkeye","Grey", 6),
+  ("Wanda Maximoff","Dark Red", 5),
+  ("Black Panther","Purple", 4),
+  ("War Machine","Black", 7),
+  ("Spider Man", "Blue", 7);
