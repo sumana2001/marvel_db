bdf652b63c2218b1ec6ec14333cace676297563e
sumana2001 <sumana.basu2001@gmail.com>
2022-03-08 12:12:16 +0530

    Minor Fixes

--- scripts/create_table.sql
+++ scripts/create_table.sql
@@ -1,10 +1,16 @@
 CREATE DATABASE IF NOT EXISTS `marvel` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+GO
 USE `marvel`;
+GO
 CREATE TABLE `superheroes` (
   `superhero_id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL DEFAULT ''
+  `name` varchar(255) NOT NULL DEFAULT '',
   `color` varchar(30) NOT NULL,
   `noOfMovies` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+GO
 ALTER TABLE `superheroes`
-  MODIFY `superheroes` int(11) NOT NULL AUTO_INCREMENT;
+  ADD PRIMARY KEY (`superhero_id`);
+GO
+ALTER TABLE `superheroes`
+  MODIFY `superhero_id` int(11) NOT NULL AUTO_INCREMENT;
